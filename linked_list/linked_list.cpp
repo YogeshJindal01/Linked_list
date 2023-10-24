bb4baab4450f2289b71307bec65df3a092960141
Yogesh Jindal <126373706+YogeshJindal01@users.noreply.github.com>
2023-10-24 22:22:43 +0530

    Update linked_list.cpp

--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -634,6 +634,79 @@ using namespace std;
 
 //     deletenode(head,3);
 //     print(head);
-
 // }
 
+
+
+
+//delete node form last.
+// class Node{
+//     public:
+//     int data;
+//     Node* next;
+
+//     Node(int data){
+//         this -> data=data;
+//         this -> next=NULL;
+//     }
+// };
+// void insertHead(Node* &head,int d){
+//     //new node create
+//     Node* temp=new Node(d);  //as new data is passed new node will be created.
+//     temp -> next=head;
+//     head=temp;
+// }
+// void inserttail(Node* &tail,int d){
+//     Node* temp=new Node(d);
+//     tail -> next=temp;
+//     tail = temp;
+// }
+// int print(Node* &tail){
+//     Node* temp=tail;
+//     int count=0;
+//     while(temp != NULL){
+//         cout<< temp -> data <<" ";
+//         temp=temp -> next;
+//         count++;
+//     }
+//     return count;
+// }
+// Node* deletenode(Node* &head,int position,int size){
+
+//     Node* temp=head;
+//     int cnt=1;
+//     int step=(size-position)+1;
+//     while(temp!=NULL && cnt<step){
+//         temp=temp->next;
+//         cnt++;
+//     }
+//     Node* prev=NULL;
+//     Node* curr=head;
+//     while(curr!=NULL && curr!=temp){
+//         prev=curr;
+//         curr=curr->next;
+//     }
+//     prev->next=curr->next;
+//     curr->next=NULL;
+
+//     return head;
+// }
+// int main(){
+
+//     Node* node1=new Node(1);
+
+//     Node* head=node1;
+//     Node* tail=node1;
+
+
+//     inserttail(tail,2);
+//     inserttail(tail,3);
+//     inserttail(tail,4);
+//     inserttail(tail,5);
+
+//     print(head);
+//     cout<<endl;
+
+//     deletenode(head,2,5);
+//     print(head);
+// }
